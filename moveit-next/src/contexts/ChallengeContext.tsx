2eb7382fb3fdc5c87fef6e7177e8515ac98c6760
Celso <celsogubeal@gmail.com>
2021-02-26 12:26:32 -0300

    Using context

--- moveit-next/src/contexts/ChallengeContext.tsx
+++ moveit-next/src/contexts/ChallengeContext.tsx
@@ -1,3 +1,17 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const ChallengesContext = createContext({});
+
+export function ChallengesProvider({children}) {
+  const [level, setLevel] = useState(1);
+  
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  return (
+    <ChallengesContext.Provider value={{level, levelUp}}>
+      {children}
+    </ChallengesContext.Provider>
+  );
+}
